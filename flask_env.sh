7493a1a8c54eb250e8bca71655e99302f7c930d4
sergio <sergioli212@outlook.com>
2018-12-01 16:32:15 +1100

    install database-related package for flask environment

---
+++ flask_env.sh
@@ -0,0 +1,9 @@
+#database-related package 
+pip install flask-sqlacodegen
+conda install flask-sqlalchemy
+
+
+apt install libmysqlclient-dev
+apt install gcc
+apt install python-dev
+conda install mysqlclient
